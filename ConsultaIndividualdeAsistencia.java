799be170841267b6493833d47fd833d260a44403
Raul Perez <raulpmcr@outlook.com>
2025-11-28 12:15:00 -0600

    Update and rename Principal.java to ConsultaIndividualdeAsistencia.java

---
+++ ConsultaIndividualdeAsistencia.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/**
+ *
+ * @author Raul
+ public class ConsultaIndividualdeAsistencia {
+    
+}
